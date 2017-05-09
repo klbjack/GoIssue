d6d054a56f769692729752a03e4273af82b45653
klbjack <kjackson5595@att.net>
2017-05-09 16:52:05 -0400

    Kev's Update (#1)
    * test
    
    * Update 1

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
+	session, err := mgo.Dial("server1.example.com,server2.example.com")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
 	fmt.Printf("hello, world\n")
 
+	//test
 }
